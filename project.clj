62dab2ca11173e5c41f703a8e8b222b87d9acd35
mladvladimir <mlad.vladimir@gmail.com>
2018-11-09 19:24:47 +0100

    add project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.vladimir/aristotle "0.0.1-SNAPSHOT"
+  :description "aristotle - fork"
+  :url "https://github.com/arachne-framework/aristotle"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.apache.jena/apache-jena-libs "3.4.0" :extension "pom"]])
