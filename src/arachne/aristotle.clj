a4aa45d13d81a68e0e740f464af7685102e69fae
Djordje Mutavdzic <muta93srb@hotmail.com>
2021-06-18 12:06:24 +0200

    Write function added, apache jena updated to 4.1.0

--- src/arachne/aristotle.clj
+++ src/arachne/aristotle.clj
@@ -8,7 +8,7 @@
             [clojure.java.io :as io])
   (:import [org.apache.jena.reasoner.rulesys GenericRuleReasoner]
            [org.apache.jena.graph Factory Graph GraphUtil]
-           [org.apache.jena.riot RDFDataMgr]
+           [org.apache.jena.riot RDFDataMgr RDFFormat]
            [java.net URL]
            [java.io File])
   (:refer-clojure :exclude [read]))
@@ -75,4 +75,50 @@
                                                        (str))))
   graph)
 
-
+(defn write
+  "Serialize a graph and write to file.
+  The file may be specified using:
+  - String URIs,
+  - java.net.URI,
+  - java.net.URL
+  - java.io.File
+  File types available:
+  ABBREV, BLOCKS, FLAT, NQ, NQUADS, NT, NTRIPLES, PLAIN, PRETTY, RDFJSON, RDFNULL,
+  RDFXML, RDFXML_ABBREV, RDFXML_PLAIN, RDFXML_PRETTY, TRIG, TRIG_BLOCKS, TRIG_FLAT,
+  TRIG_PRETTY, TTL, TURTLE, TURTLE_BLOCKS, TURTLE_FLAT, TURTLE_PRETTY"
+  [^Graph graph file type]
+  (let [t (cond
+            (= type "ABBREV")        (RDFFormat/ABBREV)
+            (= type "BLOCKS")        (RDFFormat/BLOCKS)
+            (= type "FLAT")          (RDFFormat/FLAT)
+            (= type "NQ")            (RDFFormat/NQ)
+            (= type "NQUADS")        (RDFFormat/NQUADS)
+            (= type "NT")            (RDFFormat/NT)
+            (= type "NTRIPLES")      (RDFFormat/NTRIPLES)
+            (= type "PLAIN")         (RDFFormat/PLAIN)
+            (= type "PRETTY")        (RDFFormat/PRETTY)
+            (= type "RDFJSON")       (RDFFormat/RDFJSON)
+            (= type "RDFNULL")       (RDFFormat/RDFNULL)
+            (= type "RDFXML")        (RDFFormat/RDFXML)
+            (= type "RDFXML_ABBREV") (RDFFormat/RDFXML_ABBREV)
+            (= type "RDFXML_PLAIN")  (RDFFormat/RDFXML_PLAIN)
+            (= type "RDFXML_PRETTY") (RDFFormat/RDFXML_PRETTY)
+            (= type "TRIG")          (RDFFormat/TRIG)
+            (= type "TRIG_BLOCKS")   (RDFFormat/TRIG_BLOCKS)
+            (= type "TRIG_FLAT")     (RDFFormat/TRIG_FLAT)
+            (= type "TRIG_PRETTY")   (RDFFormat/TRIG_PRETTY)
+            (= type "TTL")           (RDFFormat/TTL)
+            (= type "TURTLE")        (RDFFormat/TURTLE)
+            (= type "TURTLE_BLOCKS") (RDFFormat/TURTLE_BLOCKS)
+            (= type "TURTLE_FLAT")   (RDFFormat/TURTLE_FLAT)
+            (= type "TURTLE_PRETTY") (RDFFormat/TURTLE_PRETTY))])
+  (with-open [o (io/output-stream
+                  (cond
+                    (string? file) file
+                    (uri? file) (str file)
+                    (instance? java.net.URL file) (str (.toURI ^URL file))
+                    (instance? java.io.File file) (-> ^File file
+                                                      (.getAbsoluteFile)
+                                                      (.toURI)
+                                                      (str))))]
+    (RDFDataMgr/write o graph (RDFFormat/TTL))))
